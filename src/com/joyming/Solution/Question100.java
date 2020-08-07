8ee712586e60b5065ba3039b0796ebfc989a419c
Joy Chen <joymingchen@gmail.com>
2020-08-07 16:07:45 +0800

    100. 相同的树 增加树的数据结构；递归判断树的所有子结点

---
+++ src/com/joyming/Solution/Question100.java
@@ -0,0 +1,63 @@
+package com.joyming.Solution;
+
+import com.joyming.data.TreeNode;
+
+/**
+ * 100. 相同的树
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:       1         1
+ * / \       / \
+ * 2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
+ * <p>
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:      1          1
+ * /           \
+ * 2             2
+ * <p>
+ * [1,2],     [1,null,2]
+ * <p>
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入:       1         1
+ * / \       / \
+ * 2   1     1   2
+ * <p>
+ * [1,2,1],   [1,1,2]
+ * <p>
+ * 输出: false
+ */
+public class Question100 {
+
+    /**
+     * 递归
+     * 深度优先搜索
+     * 时间复杂度：O(min(m,n))，
+     * 其中 mm 和 nn 分别是两个二叉树的节点数。对两个二叉树同时进行深度优先搜索，
+     * 只有当两个二叉树中的对应节点都不为空时才会访问到该节点，因此被访问到的节点数不会超过较小的二叉树的节点数。
+     *
+     * 空间复杂度：O(min(m,n))，
+     * 其中 mm 和 nn 分别是两个二叉树的节点数。空间复杂度取决于递归调用的层数，
+     * 递归调用的层数不会超过较小的二叉树的最大高度，最坏情况下，二叉树的高度等于节点数。
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
