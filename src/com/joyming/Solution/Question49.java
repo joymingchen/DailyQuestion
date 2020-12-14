0bc438dfbdfa05aa778ca61b600f8805876facfa
Joy Chen <joymingchen@gmail.com>
2020-12-14 16:06:21 +0800

    49. 字母异味词分组(Medium)

---
+++ src/com/joyming/Solution/Question49.java
@@ -0,0 +1,55 @@
+package com.joyming.Solution;
+
+import java.util.*;
+
+/**
+ * 49. 字母异位词分组
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ * <p>
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ */
+public class Question49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> answer = new ArrayList<>();
+        List<List<String>> sortAnswer = new ArrayList<>();
+        String[] test = strs.clone();
+        for (int i = 0; i < test.length; i++) {
+            String item = test[i];
+            char[] chars = item.toCharArray();
+            Arrays.sort(chars);
+            test[i] = String.valueOf(chars);
+        }
+        for (int i = 0; i < test.length; i++) {
+            String item = test[i];
+            boolean isHave = false;
+            for (int j = 0; j < answer.size(); j++) {
+                if (sortAnswer.get(j).contains(item)) {
+                    isHave = true;
+                    sortAnswer.get(j).add(test[i]);
+                    answer.get(j).add(strs[i]);
+                }
+            }
+            if (!isHave) {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                answer.add(list);
+                List<String> sortList = new ArrayList<>();
+                sortList.add(test[i]);
+                sortAnswer.add(sortList);
+            }
+        }
+        return answer;
+    }
+
+}
