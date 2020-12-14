a218ceb2eaaa1fad08564e5f70e0fac71779a8fc
Joy Chen <joymingchen@gmail.com>
2020-12-14 16:06:54 +0800

    49. 字母异味词分组(Medium) 增加hash表分组的方法

--- src/com/joyming/Solution/Question49.java
+++ src/com/joyming/Solution/Question49.java
@@ -52,4 +52,20 @@ public class Question49 {
         return answer;
     }
 
+    /**
+     * 利用hash表
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key,new ArrayList<>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
