f32fb3a547fff05dc6b3a6c306d8ad85a53a7852
Joy Chen <joymingchen@gmail.com>
2021-01-21 18:13:51 +0800

    947. 移除最多的同行或同列石头(Medium) 并查集，根据不同情况构造数据结构 求连通分量的问题

--- src/com/joyming/Main.java
+++ src/com/joyming/Main.java
@@ -80,10 +80,10 @@ public class Main {
 
         //[[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]
         int[][] A2 = new int[][]{{1,0,0,1}, {0,1,1,0}, {0,1,1,1}, {1,0,1,1}};
-        int[][] A3 = new int[][]{{0,0},{1,1},{1,0},{-1,1}};
+        int[][] A3 = new int[][]{{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}};
         int[] B2 = new int[]{-100,-98,-1,2,3,4};
         int[] B3 = new int[]{1,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,1,1,1,1,1,1,0,0,0,1,0,1,1,1,1,0,1,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,1,1,0,0,1,1,1};
-        System.out.print(new Question1584().minCostConnectPoints(A3));
+        System.out.print(new Question947().removeStones(A3));
 
         long endTime = System.currentTimeMillis();
         System.out.println("\n\n执行时间:" + (endTime - startTime) + "ms");
