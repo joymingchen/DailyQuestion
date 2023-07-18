4ceb3c5a00e6cb3e656b8eae862e15a46bfc4a55
Joy <joymingchen@gmail.com>
2023-07-18 13:36:05 +0800

    151. 反转字符串中的单词

--- src/com/joyming/Main.java
+++ src/com/joyming/Main.java
@@ -1,9 +1,6 @@
 package com.joyming;
 
-import com.joyming.Solution.*;
-import com.joyming.Data.TreeNode;
-
-import java.util.Arrays;
+import com.joyming.Solution.Question151;
 
 public class Main {
 
@@ -12,10 +9,10 @@ public class Main {
 
 
 //        String[] test = {"flower","flow","fl"};
-        String test = "babad";
+        String test = "a good  example";
         System.out.println(test);
 //        System.out.println(Arrays.toString(test));
-        String result = new Question5().longestPalindrome4(test);
+        String result = new Question151().reverseWords(test);
         System.out.println(result);
 
         long endTime = System.currentTimeMillis();
---
+++ src/com/joyming/Solution/Question151.java
@@ -0,0 +1,72 @@
+package com.joyming.Solution;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 151. 反转字符串中的单词
+ * 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+ * <p>
+ * 单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+ * <p>
+ * 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+ * <p>
+ * 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "the sky is blue"
+ * 输出："blue is sky the"
+ * 示例 2：
+ * <p>
+ * 输入：s = "  hello world  "
+ * 输出："world hello"
+ * 解释：反转后的字符串中不能存在前导空格和尾随空格。
+ * 示例 3：
+ * <p>
+ * 输入：s = "a good   example"
+ * 输出："example good a"
+ * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 包含英文大小写字母、数字和空格 ' '
+ * s 中 至少存在一个 单词
+ * <p>
+ * <p>
+ * 进阶：如果字符串在你使用的编程语言中是一种可变数据类型，请尝试使用 O(1) 额外空间复杂度的 原地 解法。
+ */
+public class Question151 {
+
+    public String reverseWords(String s) {
+        //删除首尾空格
+        s = s.trim();
+        String[] strings = s.split(" ");
+        StringBuilder answer = new StringBuilder();
+        for (int i = strings.length - 1; i >= 0; i--) {
+            if (strings[i].equals("")) {
+                continue;
+            }
+            answer.append(strings[i].trim());
+            answer.append(" ");
+        }
+        return answer.toString().trim();
+    }
+
+
+    public String reverseWords2(String s) {
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+
+}
