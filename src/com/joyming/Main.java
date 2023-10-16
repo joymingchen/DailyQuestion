2bacfce6760243857be9a24ea93af69f6a18366b
Joy <joymingchen@gmail.com>
2023-10-16 14:21:53 +0800

    260. 只出现一次的数字 III

--- src/com/joyming/Main.java
+++ src/com/joyming/Main.java
@@ -1,6 +1,9 @@
 package com.joyming;
 
 import com.joyming.Solution.Question151;
+import com.joyming.Solution.Question260;
+
+import java.util.Arrays;
 
 public class Main {
 
@@ -10,9 +13,10 @@ public class Main {
 
 //        String[] test = {"flower","flow","fl"};
         String test = "a good  example";
+        int[] nums = new int[]{1,2,1,3,2,5};
         System.out.println(test);
 //        System.out.println(Arrays.toString(test));
-        String result = new Question151().reverseWords(test);
+        String result = Arrays.toString(new Question260().singleNumber(nums));
         System.out.println(result);
 
         long endTime = System.currentTimeMillis();
