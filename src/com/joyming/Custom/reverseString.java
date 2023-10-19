e9f45516ef7ec33eb3b1ef32bcf16c90cf062a4f
Joy <joymingchen@gmail.com>
2023-10-19 17:46:00 +0800

    反转字符串

---
+++ src/com/joyming/Custom/reverseString.java
@@ -0,0 +1,41 @@
+package com.joyming.Custom;
+
+/**
+ * 反转字符串
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+ *
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ *
+ *
+ * 示例 1：
+ * 输入：s = ["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ *
+ * 示例 2：
+ * 输入：s = ["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ *
+ * 提示：
+ * 1 <= s.length <= 105
+ * s[i] 都是 ASCII 码表中的可打印字符
+ *
+ *
+ *
+ * 作者：LeetCode
+ * 链接：https://leetcode.cn/leetbook/read/array-and-string/cacxi/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class reverseString {
+
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
