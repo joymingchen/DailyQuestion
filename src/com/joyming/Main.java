b06f8173d324a089fd4742fe6a334427ac67fc00
Joy <joymingchen@gmail.com>
2023-10-23 15:57:59 +0800

    26. 删除有序数组中的重复项

--- src/com/joyming/Main.java
+++ src/com/joyming/Main.java
@@ -16,13 +16,13 @@ public class Main {
 //        String haystack = "aabaaabaaac", needle = "aabaaac";
 //        String haystack = "ACTGPACTGKACTGPACY", needle = "ACTGPACY";
 //        int[] nums = new int[]{881784984,829998714,730002802,56524562,120336848,548306998,801116106,828640251,519131180,819176153,476262254,15904939,540793851,53572296,259044378,491129827,561147559,205793082,967833729};
-        int[] nums = new int[]{2,3,1,2,4,3};
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
         int target = 7;
 //        int n = 16;
         int n = 3;
         System.out.println(test);
 //        System.out.println(Arrays.toString(test));
-        String result = String.valueOf(new Question119().getRow(3));
+        String result = String.valueOf(new Question26().removeDuplicates(nums));
         System.out.println(result);
 
         long endTime = System.currentTimeMillis();
